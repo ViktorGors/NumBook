306ad2c59066050662cdb05df013dcc096d3c1aa
ViktorGors <90250275+ViktorGors@users.noreply.github.com>
2022-03-19 12:50:27 +0300

    Initial commit

---
+++ src/main/kotlin/NumBook.kt
@@ -0,0 +1,111 @@
+
+class NumBook {
+
+    var NumBook = HashMap<String, MutableSet<String>>()
+
+    fun СorrectIncorrectNum(num: String) {
+        if (!num.matches(Regex("[0-9+*\\-#]+"))) throw IllegalArgumentException("Incorrect number! Please check the data entry.")
+    }
+    fun СorrectIncorrectName(name: String) {
+        if (!name.matches(Regex("[А-ЯЁ][а-яё]+\\s[А-ЯЁ][а-яё]+"))) throw IllegalArgumentException("Incorrect name! Please check the data entry.")
+    }
+
+    // Добавить человека.
+    // true - человек добавлен.
+    // false - если человек с таким именем уже был.
+
+    fun addName(name: String): Boolean {
+        СorrectIncorrectName(name) // проверочка
+        return if (NumBook.containsKey(name))
+            false
+        else {
+            NumBook[name] = mutableSetOf()
+            true
+        }
+    }
+
+    // Добавить номер.
+    // true - номер был добавлен.
+    // false - человек с таким именем отсутствовал / у него уже был такой номер / такой номер телефона уже был зарегистрирован.
+
+    fun addNum(name: String, num: String): Boolean {
+
+        СorrectIncorrectNum(num) // проверочка
+        СorrectIncorrectName(name) // проверочка
+
+        if (!NumBook.containsKey(name)) return false
+        val Number = NumBook[name]
+        if (Number!!.contains(num)) return false //проверка на наличие номера в телефонной книге
+        for (personNum in NumBook.values) { //проверка на наличие этого номера у других людей
+            if (personNum.contains(num)) return false
+        }
+        Number.add(num)
+        return true
+    }
+
+    // Удалить имя.
+    // true - имя удалено.
+    // false - человека с таким именем не существует
+
+    fun delName(name: String): Boolean {
+        СorrectIncorrectName(name) // проверочка
+        return if (!NumBook.containsKey(name)) false
+        else {
+            NumBook.remove(name)
+            true
+        }
+    }
+
+    // Удалить номер.
+    // true - номер удалён.
+    // false - человек с таким именем не существует (либо у него не было такого номера).
+
+    fun delNum(name: String, num: String): Boolean {
+        СorrectIncorrectNum(num) // проверочка
+        СorrectIncorrectName(name) // проверочка
+        if (!NumBook.containsKey(name)) return false
+        val phones = NumBook[name]
+        if (!phones!!.contains(num)) return false
+        phones.remove(num)
+        return true
+    }
+
+    // Вернуть все номера телефона заданного человека.
+    // В ином случае, вернуть пустой список
+
+    fun numbers(name: String): Set<String> {
+        СorrectIncorrectName(name) // проверочка
+        return NumBook[name] ?: setOf() //пустое множество при null
+    }
+
+    // Вернуть имя по номеру.
+    // В ином случае, вернуть null.
+
+    fun nameByNum(num: String): String? {
+        СorrectIncorrectNum(num) // проверочка
+        for ((name, phones) in NumBook) {
+            if (phones.contains(num)) return name
+        }
+        return null
+    }
+
+
+    // Две книги равны, если в них находится одинаковый набор людей,
+    // одинаковый набор телефонов. А, порядок в книге не должен иметь значения.
+
+    override fun equals(other: Any?): Boolean {
+        if (other is NumBook) {
+            if (other.NumBook.size != this.NumBook.size) return false
+            for ((name, num) in NumBook) {
+                if (other.NumBook[name] != num) return false
+            }
+        }
+        return true
+    }
+
+
+    override fun hashCode(): Int = NumBook.hashCode()
+
+}
+
+
---
+++ src/test/kotlin/NumBookTest.kt
@@ -0,0 +1,149 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class NumBookTest {
+
+    @Test
+    fun addName() {
+        val book = NumBook()
+        assertTrue(book.addName("Иосиф Сталин"))
+        assertTrue(book.addName("Васильев Дмитрий"))
+        assertFalse(book.addName("Иосиф Сталин"))
+
+    }
+
+
+
+
+
+    @Test
+    fun addNum() {
+        val book = NumBook()
+        assertTrue(book.addName("Ильич Ленин"))
+        assertTrue(book.addName("Бобрышев Дмитрий"))
+        assertTrue(book.addNum("Ильич Ленин", "+79212345678"))
+        assertTrue(book.addNum("Ильич Ленин", "+78121234567"))
+        assertFalse(book.addNum("Ильич Ленин", "+79212345678"))
+        assertFalse(book.addNum("Бобрышев Дмитрий", "+79212345678"))
+        assertTrue(book.addNum("Бобрышев Дмитрий", "+79217654321"))
+        assertTrue(book.addNum("Ильич Ленин", "+7+9+123434567"))
+        assertTrue(book.addNum("Ильич Ленин", "+792*1123#456-7"))
+        //assertTrue(book.addNum("Ильич Ленин", "+792*1123=456-7"))
+        //assertThrows(IllegalArgumentException::class.java) { book.addName("+7923211=1234") }
+    }
+
+    @Test
+    fun delName() {
+        val book = NumBook()
+        assertTrue(book.addName("Ильич Ленин"))
+        assertTrue(book.addName("Иосиф Виссарионович"))
+        assertTrue(book.addName("Берия Лаврентий"))
+        assertTrue(book.delName("Ильич Ленин"))
+        assertFalse(book.delName("Троцкий Лев"))
+    }
+
+    @Test
+    fun delNum() {
+        val book = NumBook()
+        assertTrue(book.addName("Ильич Ленин"))
+        assertTrue(book.addName("Иосиф Виссарионович"))
+        assertTrue(book.addNum("Ильич Ленин", "+79212121212"))
+        assertTrue(book.addNum("Ильич Ленин", "+78187654321"))
+        assertTrue(book.addNum("Иосиф Виссарионович", "+79222222222"))
+        assertTrue(book.delNum("Ильич Ленин", "+78187654321"))
+        assertFalse(book.delNum("Ильич Ленин", "+78187654321"))
+        assertTrue(book.delNum("Иосиф Виссарионович", "+79222222222"))
+    }
+
+    @Test // Делай
+    fun numbers() {
+        val book = NumBook()
+        assertTrue(book.addName("Ильич Ленин"))
+        assertTrue(book.addName("Иосиф Виссарионович"))
+        assertTrue(book.addNum("Ильич Ленин", "+79212345678"))
+        assertTrue(book.addNum("Ильич Ленин", "+78187654321"))
+        assertEquals(setOf("+79212345678", "+78187654321"), book.numbers("Ильич Ленин"))
+    }
+
+    @Test
+    fun nameByNum() {
+        val book = NumBook()
+        assertTrue(book.addName("Ильич Ленин"))
+        assertTrue(book.addName("Иосиф Виссарионович"))
+        assertTrue(book.addNum("Ильич Ленин", "+79211110000"))
+        assertTrue(book.addNum("Ильич Ленин", "+79276192492"))
+        assertTrue(book.addNum("Иосиф Виссарионович", "+79217654321"))
+        assertEquals("Иосиф Виссарионович", book.nameByNum("+79217654321"))
+        assertEquals("Ильич Ленин", book.nameByNum("+79276192492"))
+        assertNull(book.nameByNum("+79204030201"))
+    }
+
+    @Test
+    fun testEquals() {
+        val bookA = NumBook()
+        assertTrue(bookA.addName("Иосиф Виссарионович"))
+        assertTrue(bookA.addName("Ильич Ленин"))
+        assertTrue(bookA.addNum("Ильич Ленин", "+79276192492"))
+        assertTrue(bookA.addNum("Иосиф Виссарионович", "+79217654321"))
+        assertTrue(bookA.addNum("Ильич Ленин", "+79212345678"))
+
+        val bookB = NumBook()
+        assertTrue(bookB.addName("Ильич Ленин"))
+        assertTrue(bookB.addName("Иосиф Виссарионович"))
+        assertTrue(bookB.addNum("Ильич Ленин", "+79212345678"))
+        assertTrue(bookB.addNum("Ильич Ленин", "+79276192492"))
+        assertTrue(bookB.addNum("Иосиф Виссарионович", "+79217654321"))
+        assertTrue(bookB == bookA)
+
+        val bookC = NumBook()
+        assertTrue(bookC.addName("Иосиф Виссарионович"))
+        assertTrue(bookC.addName("Ильич Ленин"))
+        assertTrue(bookC.addNum("Ильич Ленин", "+79217654321"))
+        assertTrue(bookC.addNum("Иосиф Виссарионович", "+79276192492"))
+        assertTrue(bookC.addNum("Ильич Ленин", "+79212345678"))
+        assertFalse(bookA == bookC)
+
+        val bookD = NumBook()
+        assertTrue(bookD.addName("Иосиф Виссарионович"))
+        assertTrue(bookD.addName("Ильич Ленин"))
+        assertFalse(bookC == bookD)
+    }
+
+    @Test
+    fun testHashCode() {
+        val bookA = NumBook()
+        assertTrue(bookA.addName("Ильич Ленин")) //1
+        assertTrue(bookA.addName("Иосиф Виссарионович")) //3
+        assertTrue(bookA.addNum("Ильич Ленин", "+79212345678")) //5
+        assertTrue(bookA.addNum("Ильич Ленин", "+79276192492")) //6
+        assertTrue(bookA.addNum("Ильич Ленин", "+78187654321")) //2
+        assertTrue(bookA.addNum("Иосиф Виссарионович", "+79217654321")) //4
+
+        val bookB = NumBook()
+        assertTrue(bookB.addName("Иосиф Виссарионович")) //3
+        assertTrue(bookB.addName("Ильич Ленин")) //1
+        assertTrue(bookB.addNum("Ильич Ленин", "+78187654321")) //2
+        assertTrue(bookB.addNum("Иосиф Виссарионович", "+79217654321")) //4
+        assertTrue(bookB.addNum("Ильич Ленин", "+79212345678")) //5
+        assertTrue(bookB.addNum("Ильич Ленин", "+79276192492")) //6
+        assertTrue(bookA.hashCode() == bookB.hashCode())
+
+        val bookC = NumBook()
+        assertTrue(bookC.addName("Иосиф Виссарионович"))
+        assertTrue(bookC.addName("Ильич Ленин"))
+        assertTrue(bookC.addNum("Ильич Ленин", "+79217654321"))
+        assertTrue(bookC.addNum("Иосиф Виссарионович", "+78187654321"))
+        assertTrue(bookC.addNum("Ильич Ленин", "+79212345678"))
+        assertFalse(bookA.hashCode() == bookC.hashCode())
+
+        val bookD = NumBook()
+        assertTrue(bookD.addName("Иосиф Виссарионович"))
+        assertTrue(bookD.addName("Ильич Ленин"))
+        assertTrue(bookD.addName("Троцкий Лев"))
+        assertTrue(bookD.addNum("Ильич Ленин", "+79276192492"))
+        assertTrue(bookD.addNum("Троцкий Лев", "+79206669990"))
+        assertFalse(bookC.hashCode() == bookD.hashCode())
+    }
+
+    }
